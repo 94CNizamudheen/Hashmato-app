1423ef3ea675f23a181ad433de8cdf9be19207a4
Nizamudheen <nisunisam2@gmail.com>
2025-09-21 14:10:11 +0530

    env excepton

--- hashmato-app/tauri-app/src/services/api.ts
+++ hashmato-app/tauri-app/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = import.meta.env.VITE_API_BASE ;
+export const API_BASE = import.meta.env.VITE_API_BASE || "https://hashmato-app.onrender.com";
 
 const client = axios.create({
   baseURL: API_BASE,
